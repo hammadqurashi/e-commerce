745f3d9193c598c2494a4aba111f7661cfb82fb2
hammad <hammadqurashi103@gmail.com>
2025-09-02 03:04:10 +0500

    Update products.tsx

--- frontend/src/pages/public/products.tsx
+++ frontend/src/pages/public/products.tsx
@@ -38,10 +38,6 @@ export default function Products() {
     []
   );
 
-  if (isFetching) {
-    return <PageSpinner />;
-  }
-
   const { products = [], totalPages = 0 } = data || {};
 
   return (
@@ -62,14 +58,24 @@ export default function Products() {
             </p>
           </div>
 
-          {products.length > 0 && (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 min-h-[400px]">
-              {products.map((product) => (
-                <ProductCard key={product._id} product={product} />
-              ))}
-            </div>
+          {!isFetching && products.length > 0 && (
+            <>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 min-h-[400px]">
+                {products.map((product) => (
+                  <ProductCard key={product._id} product={product} />
+                ))}
+              </div>
+
+              <div className="my-12">
+                <ProductsPagination
+                  currentPage={currentPage}
+                  onPageChange={handlePageChange}
+                  totalPages={totalPages}
+                />
+              </div>
+            </>
           )}
-          {products.length <= 0 && (
+          {!isFetching && products.length <= 0 && (
             <div className="container mx-auto px-4 py-8 min-h-[400px]">
               <div className="text-center py-16">
                 <h1 className="font-heading text-2xl font-medium mb-4">
@@ -82,15 +88,7 @@ export default function Products() {
             </div>
           )}
 
-          {products.length > 0 && (
-            <div className="my-12">
-              <ProductsPagination
-                currentPage={currentPage}
-                onPageChange={handlePageChange}
-                totalPages={totalPages}
-              />
-            </div>
-          )}
+          {isFetching && <PageSpinner />}
         </main>
       </div>
     </div>
