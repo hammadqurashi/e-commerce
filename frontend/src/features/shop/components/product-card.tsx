da0341f42fd8e66e676d6bd0edd9391f5e3dbb6b
hammad <hammadqurashi103@gmail.com>
2025-09-02 02:30:35 +0500

    enhanced product card

--- frontend/src/features/shop/components/product-card.tsx
+++ frontend/src/features/shop/components/product-card.tsx
@@ -9,22 +9,22 @@ interface ProductCardProps {
 const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <Link to={`/product/${product.slug}`}>
-      <Card className="group cursor-pointer border-0 shadow-none bg-product-card hover:bg-product-card-hover transition-colors">
+      <Card className="group cursor-pointer shadow-none border py-0 bg-product-card hover:bg-product-card-hover transition-colors">
         <CardContent className="p-0">
-          <div className="aspect-square overflow-hidden rounded-sm mb-3">
+          <div className="aspect-square overflow-hidden rounded-lg ">
             <img
               src={product.images[0]}
               alt={product.name}
               className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
             />
           </div>
-          <div className="space-y-1">
-            <h3 className="font-body text-sm font-medium text-foreground leading-tight">
+          <div className="space-y-1 p-4">
+            <h3 className=" font-semibold text-foreground leading-tight">
               {product.name}
             </h3>
             <div className="flex items-center gap-2">
               <span className="font-body text-sm font-medium text-price">
-                $ {product.price.toLocaleString()}
+                ${product.price.toLocaleString()}
               </span>
             </div>
           </div>
